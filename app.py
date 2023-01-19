611594bd10d5f383fb3b34f84af22ac5910b0279
Yemoe Aung <108300573+yemoeaung1@users.noreply.github.com>
2023-01-18 23:09:01 -0500

    Make the result appear on the same page

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import NumberConvert
 
 app = Flask(__name__)
-    
+
 
 @app.route("/", methods = ["GET", "POST"])
 def convert():
-    if request.method == "POST":
-        amount = request.form.get("amount")
-        print(amount)
-        return NumberConvert.mainConvert(amount)
-    return render_template("index.html")
-    
+    try:
+        result = " "
+        if request.method == "POST":
+            amount = request.form.get("amount")
+            result = NumberConvert.mainConvert(amount)
+        return render_template("index.html", result = result)
+    except ValueError():
+        return redirect('index.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
